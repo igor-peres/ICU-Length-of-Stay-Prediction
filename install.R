85cbf7589ad78a0c07a7923e33b816e917cc1b96
Joana da Matta <108022074+jodamatta@users.noreply.github.com>
2025-06-09 13:58:26 -0300

    update notebooks for binder

--- install.R
+++ install.R
@@ -1,7 +1,20 @@
-if (!require("caret")) {install.packages("caret", dependencies = TRUE) ; library(caret)}
-if (!require("caretEnsemble")) {install.packages("caretEnsemble", dependencies = TRUE) ; library(caretEnsemble)}
-if (!require("tidyverse")) {install.packages("tidyverse") ; library(tidyverse)}
-if (!require("MLmetrics")) {install.packages("MLmetrics") ; library(MLmetrics)}
-if (!require("ranger")) {install.packages("ranger"); library(ranger) }
-if (!require("DescTools")) {install.packages("DescTools"); library(DescTools) }
-if (!require("mice")) {install.packages("mice"); library(mice) }
+packages <- c(
+  "caret",
+  "caretEnsemble",
+  "tidyverse",
+  "MLmetrics",
+  "ranger",
+  "DescTools",
+  "mice",
+  "IRkernel"
+)
+
+installed <- rownames(installed.packages())
+
+for (pkg in packages) {
+  if (!pkg %in% installed) {
+    install.packages(pkg, dependencies = TRUE)
+  }
+}
+
+IRkernel::installspec(user = FALSE)
