ef537a81b4c94a853fb175f952af26e0b030189a
Joana da Matta <108022074+jodamatta@users.noreply.github.com>
2025-06-09 13:47:57 -0300

    update notebooks for binder

---
+++ install.R
@@ -0,0 +1,7 @@
+if (!require("caret")) {install.packages("caret", dependencies = TRUE) ; library(caret)}
+if (!require("caretEnsemble")) {install.packages("caretEnsemble", dependencies = TRUE) ; library(caretEnsemble)}
+if (!require("tidyverse")) {install.packages("tidyverse") ; library(tidyverse)}
+if (!require("MLmetrics")) {install.packages("MLmetrics") ; library(MLmetrics)}
+if (!require("ranger")) {install.packages("ranger"); library(ranger) }
+if (!require("DescTools")) {install.packages("DescTools"); library(DescTools) }
+if (!require("mice")) {install.packages("mice"); library(mice) }
